649af88d5c1942dc58d9e6b7492d8bc921af80fc
lienne <lianasevero3@gmail.com>
2017-11-12 01:01:52 -0500

    Doesn't break linux

--- LiveCode.py
+++ LiveCode.py
@@ -8,56 +8,62 @@ import os
 isLinux = sys.platform == 'linux'
 
 try:
-	import tty, termios
-except:
-	import msvcrt
-
-def clr():
 	if isLinux:
-		os.system('clear')
+		import tty, termios
+		old_settings = termios.tcgetattr(sys.stdin)
+		tty.setcbreak(sys.stdin.fileno())
 	else:
-		os.system('cls')
-
-def getchar():
-	try:
-		fd = sys.stdin.fileno()
-		old_settings = termios.tcgetattr(fd)
-		tty.setraw(sys.stdin.fileno())
-		ch = sys.stdin.read(1)
-	except:
-		ch = msvcrt.getch()
-		try:
-			ch = ch.decode()
-		except:
-			ch = '0'
-	return ch
-
-
-if len(sys.argv)!=2:
-	print (len(sys.argv))
-	sys.exit("Incorrect Number of Arguments")
-
-with open(sys.argv[1], "r") as f:
-
-	code = f.read()
-
-clr()
-
-ch='0'
-while (ch != '\x1b'):
-	ch = getchar()
-
-	if (len(code)>1):
-		next=code[0]
-		code=code[1:]
-
-		if (next == "\r" or next=="\n"):
-			print("\r")
-
-
+		import msvcrt
+	
+	def clr():
+		if isLinux:
+			os.system('clear')
 		else:
-			print(next, end="")
-#			sys.stdout.write(next)
-			sys.stdout.flush()
+			os.system('cls')
 
-clr()
+	def getchar():
+		try:
+			fd = sys.stdin.fileno()
+			old_settings = termios.tcgetattr(fd)
+			tty.setraw(sys.stdin.fileno())
+			ch = sys.stdin.read(1)
+		except:
+			ch = msvcrt.getch()
+			try:
+				ch = ch.decode()
+			except:
+				ch = '0'
+		return ch
+
+
+	if len(sys.argv)!=2:
+		print (len(sys.argv))
+		sys.exit("Incorrect Number of Arguments")
+
+	with open(sys.argv[1], "r") as f:
+
+		code = f.read()
+
+	clr()
+
+	ch='0'
+	while (ch != '\x1b'):
+		ch = getchar()
+
+		if (len(code)>1):
+			next=code[0]
+			code=code[1:]
+
+			if (next == "\r" or next=="\n"):
+				print("\r")
+
+
+			else:
+				print(next, end="")
+	#			sys.stdout.write(next)
+				sys.stdout.flush()
+
+	clr()
+
+finally:
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
